c2d950fd67bdafe3d9481688b9c6ebd04b9fc949
Fernando Miranda <devnadostudio@gmail.com>
2026-04-23 19:08:24 -0600

    refactor: Se inicializan valores del modelo de datos.

--- app/src/main/java/com/nandom/heroesandvillains/domain/model/SuperheroModel.kt
+++ app/src/main/java/com/nandom/heroesandvillains/domain/model/SuperheroModel.kt
@@ -1,9 +1,9 @@
 package com.nandom.heroesandvillains.domain.model
 
 data class SuperheroModel(
-    val id: Int,
-    val name: String,
-    val imageUrl: String,
-    val powers: List<HeroPowerModel>,
-    val weaknesses: List<String>
+    val id: Int = 0,
+    val name: String = "",
+    val imageUrl: String = "",
+    val powers: List<HeroPowerModel> = listOf(),
+    val weaknesses: List<String> = listOf()
 )
