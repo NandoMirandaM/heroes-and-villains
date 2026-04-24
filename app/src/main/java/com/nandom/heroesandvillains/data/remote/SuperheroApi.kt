f115355cfe9ede4ec7ffa4a3e775320cc6f8d9a7
Fernando Miranda <devnadostudio@gmail.com>
2026-04-23 18:38:27 -0600

    feat: Se elimina client http, ya que se paso al modulo DI.

--- app/src/main/java/com/nandom/heroesandvillains/data/remote/SuperheroApi.kt
+++ app/src/main/java/com/nandom/heroesandvillains/data/remote/SuperheroApi.kt
@@ -3,11 +3,7 @@ package com.nandom.heroesandvillains.data.remote
 import com.nandom.heroesandvillains.data.dto.SuperheroDto
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.engine.android.Android
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
 
 interface SuperheroApi {
     suspend fun getHero(id: Int): SuperheroDto
@@ -24,16 +20,5 @@ class KtorSuperheroApi(
     companion object {
         private const val BASE_URL =
             "https://superheroapi.com/api/10220161311159074"
-    }
-}
-
-val client = HttpClient(Android) {
-    install(ContentNegotiation) {
-        json(
-            Json {
-                ignoreUnknownKeys = true // Se pone por si el servicio regresa datos adicionales no truene la app
-                isLenient = true
-            }
-        )
     }
 }
