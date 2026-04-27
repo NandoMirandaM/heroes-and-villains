57921a9071e4d0502b782933d5e45e47a4fe2030
Fernando Miranda <devnadostudio@gmail.com>
2026-04-27 16:27:25 -0600

    refactor: Se implementa serialización para modelo de datos interno.

--- app/src/main/java/com/nandom/heroesandvillains/domain/model/HeroPowerModel.kt
+++ app/src/main/java/com/nandom/heroesandvillains/domain/model/HeroPowerModel.kt
@@ -1,5 +1,8 @@
 package com.nandom.heroesandvillains.domain.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class HeroPowerModel(
     val label: String,
     val value: Int?
