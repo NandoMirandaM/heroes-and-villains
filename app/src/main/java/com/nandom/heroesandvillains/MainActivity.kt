6c4872da0fdaddf5236549550ce7b36b49066cd2
Fernando Miranda <devnadostudio@gmail.com>
2026-04-27 16:30:12 -0600

    refactor: se manda llamar el proyecto a su main principal.

--- app/src/main/java/com/nandom/heroesandvillains/MainActivity.kt
+++ app/src/main/java/com/nandom/heroesandvillains/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.nandom.heroesandvillains.presentation.screens.navigation.AppNavRoot
 import com.nandom.heroesandvillains.ui.theme.HeroesAndVillainsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HeroesAndVillainsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavRoot()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HeroesAndVillainsTheme {
-        Greeting("Android")
     }
 }
