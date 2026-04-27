2f807f5a0e3ff57dacffe0ed4a130a27ec7c938a
Fernando Miranda <devnadostudio@gmail.com>
2026-04-27 16:27:05 -0600

    refactor: Se implementa serialización para modelo de datos.

--- app/src/main/java/com/nandom/heroesandvillains/domain/model/SuperheroModel.kt
+++ app/src/main/java/com/nandom/heroesandvillains/domain/model/SuperheroModel.kt
@@ -1,5 +1,8 @@
 package com.nandom.heroesandvillains.domain.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class SuperheroModel(
     val id: Int = 0,
     val name: String = "",
